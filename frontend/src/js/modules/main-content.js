addf738e322bf631cba6630167f24495313d6bad
GenyLeong <genesis.leon.carbajal@gmail.com>
2018-07-22 04:06:50 -0500

    frontend

--- frontend/src/js/modules/main-content.js
+++ frontend/src/js/modules/main-content.js
@@ -1,4 +1,12 @@
 var MainContent = (function(){
-
- 
+    $('.modal').modal();
+    $('select').formSelect();
+    $('.collapsible').collapsible();
+    $(".firma").on("click", function(){
+            console.log("entro")
+            $("#fountainG").addClass("is-visible")
+            $(".ready-reload").removeClass("is-visible")
+            setTimeout(function(){ $(".ready-reload").addClass("is-visible")
+            $("#fountainG").removeClass("is-visible") }, 5000);           
+    })
 })();
